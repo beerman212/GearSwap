9d09dac5c53593ca26396697cb36b2e3e757c878
Joshua Beers <beerman.develops@gmail.com>
2019-08-15 00:05:00 -0500

    Revert "Test Idle Rule"
    This reverts commit 3b0aacc17ef90f0105279d277a0334fc2aca4a7b.

--- data/Escondita/Escondita_Pup_Gear.lua
+++ data/Escondita/Escondita_Pup_Gear.lua
@@ -289,23 +289,3 @@ function select_default_macro_book()
         set_macro_page(2, 20)
     end
 end
-
-function job_customize_idle_set(idleSet)
-	-- if pet.isvalid and pet.status == 'Engaged' and sets.midcast.Pet then
-	-- 	if state.PetWSGear.value and pet.tp and pet.tp > 999 then
-	-- 		if sets.midcast.Pet.PetWSGear and sets.midcast.Pet.PetWSGear[state.PetMode.value] then
-	-- 			idleSet = set_combine(idleSet, sets.midcast.Pet.PetWSGear[state.PetMode.value])
-	-- 		elseif sets.midcast.Pet.PetWSGear then
-	-- 			idleSet = set_combine(idleSet, sets.midcast.Pet.PetWSGear)
-	-- 		end
-	-- 	elseif state.PetEnmityGear.value then
-	-- 		local now = os.clock()
-	-- 		if sets.midcast.Pet.PetEnmityGear and ((PupFlashReady < now and buffactive['Light Maneuver']) or (PupVokeReady < now and buffactive['Fire Maneuver'])) then
-	-- 			idleSet = set_combine(idleSet, sets.midcast.Pet.PetEnmityGear)
-	-- 		end
-	-- 	end
-    -- end
-    idleset = set_combine(idleSet, sets.idle.Pet.Engaged.Overdrive)
-
-	return idleSet
-end
